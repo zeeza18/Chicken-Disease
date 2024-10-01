0491fe1e41254e8bce0e5a64f5a62de40cdaf128
zeeza18 <mdazeezulla2001@gmail.com>
2024-10-01 16:43:19 -0500

    model preparation

--- src/cnnClassifier/entity/config_entity.py
+++ src/cnnClassifier/entity/config_entity.py
@@ -10,16 +10,16 @@ class DataIngestionConfig:
     unzip_dir: Path
 
 
-# @dataclass(frozen=True)
-# class PrepareBaseModelConfig:
-#     root_dir: Path
-#     base_model_path: Path
-#     updated_base_model_path: Path
-#     params_image_size: list
-#     params_learning_rate: float
-#     params_include_top: bool
-#     params_weights: str
-#     params_classes: int
+@dataclass(frozen=True)
+class PrepareBaseModelConfig:
+    root_dir: Path
+    base_model_path: Path
+    updated_base_model_path: Path
+    params_image_size: list
+    params_learning_rate: float
+    params_include_top: bool
+    params_weights: str
+    params_classes: int
 
 
 
