52576abdfc042e988b6380e497e75c97c1d9b670
zeeza18 <mdazeezulla2001@gmail.com>
2024-10-03 09:31:03 -0500

    model training

--- src/cnnClassifier/entity/config_entity.py
+++ src/cnnClassifier/entity/config_entity.py
@@ -31,17 +31,16 @@ class PrepareCallbacksConfig:
 
 
 
-# @dataclass(frozen=True)
-# class TrainingConfig:
-#     root_dir: Path
-#     trained_model_path: Path
-#     updated_base_model_path: Path
-#     training_data: Path
-#     params_epochs: int
-#     params_batch_size: int
-#     params_is_augmentation: bool
-#     params_image_size: list
-
+@dataclass(frozen=True)
+class TrainingConfig:
+    root_dir: Path
+    trained_model_path: Path
+    updated_base_model_path: Path
+    training_data: Path
+    params_epochs: int
+    params_batch_size: int
+    params_is_augmentation: bool
+    params_image_size: list
 
 
 # @dataclass(frozen=True)
